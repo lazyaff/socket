343444fe44061298e3c6ddb8234a39218723d8c8
Faizal <faizalamri15@gmail.com>
2024-11-24 21:21:06 +0700

    .

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,35 +1,38 @@
 // app/api/connect/socket/route.js
-import { NextRequest, NextResponse } from 'next/server';
-import { io } from 'socket.io-client';
+import { NextRequest, NextResponse } from "next/server";
+import { io } from "socket.io-client";
+import fs from "fs";
 
 export async function POST(req: NextRequest) {
-	try {
-		const socket = io('http://localhost:3000');
-		await new Promise((resolve: any, reject) => {
-			socket.on('connect', () => {
-				console.log('Connected to Socket.IO server');
-				socket.emit('gyro', { data: 'New' });
-				setTimeout(() => {
-					socket.close();
-					resolve();
-				}, 100);
-			});
-		});
+    try {
+        const socket = io("http://localhost:3000");
+        await new Promise((resolve: any, reject) => {
+            socket.on("connect", async () => {
+                console.log("Connected to Socket.IO server");
+                const videoBuffer = fs.readFileSync("public/tes.mp4");
+                const base64 = videoBuffer.toString("base64");
+                socket.emit("video", "data:video/mp4;base64," + base64);
+                setTimeout(() => {
+                    socket.close();
+                    resolve();
+                }, 1000);
+            });
+        });
 
-		return NextResponse.json({
-			success: true,
-			status: 200,
-			message: 'Message sent to Socket.IO server',
-		});
-	} catch (error) {
-		console.error(error);
-		return NextResponse.json(
-			{
-				success: false,
-				status: 500,
-				message: 'Internal server error',
-			},
-			{ status: 500 }
-		);
-	}
+        return NextResponse.json({
+            success: true,
+            status: 200,
+            message: "Message sent to Socket.IO server",
+        });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            {
+                success: false,
+                status: 500,
+                message: "Internal server error",
+            },
+            { status: 500 }
+        );
+    }
 }
--- socketServer.mjs
+++ socketServer.mjs
@@ -1,62 +1,63 @@
-import { createServer } from 'node:http';
-import next from 'next';
-import { Server } from 'socket.io';
+import { createServer } from "node:http";
+import next from "next";
+import { Server } from "socket.io";
 
-const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
 let port = process.env.PORT || 3000;
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
 const startServer = (port) => {
-	app.prepare().then(() => {
-		const httpServer = createServer((req, res) => {
-			res.setHeader('Access-Control-Allow-Origin', '*');
-			res.setHeader(
-				'Access-Control-Allow-Methods',
-				'GET, POST, PUT, DELETE, OPTIONS'
-			);
-			res.setHeader(
-				'Access-Control-Allow-Headers',
-				'Content-Type, Authorization'
-			);
-			if (req.method === 'OPTIONS') {
-				res.writeHead(204);
-				res.end();
-				return;
-			}
-			handler(req, res);
-		});
+    app.prepare().then(() => {
+        const httpServer = createServer((req, res) => {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader(
+                "Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS"
+            );
+            res.setHeader(
+                "Access-Control-Allow-Headers",
+                "Content-Type, Authorization"
+            );
+            if (req.method === "OPTIONS") {
+                res.writeHead(204);
+                res.end();
+                return;
+            }
+            handler(req, res);
+        });
 
-		const io = new Server(httpServer, {
-			cors: {
-				origin: '*',
-				methods: ['GET', 'POST'],
-			},
-		});
+        const io = new Server(httpServer, {
+            cors: {
+                origin: "*",
+                methods: ["GET", "POST"],
+            },
+        });
 
-		io.on('connection', (socket) => {
-			socket.on('gyro', (data) => {
-				console.log(`Message from client:`, data);
-				io.emit('gyro', { data: data });
-			});
-		});
+        io.on("connection", (socket) => {
+            socket.on("video", (data) => {
+                // console.log(`Message from client:`, data);
+                console.log("client sent video");
+                io.emit("video", data);
+            });
+        });
 
-		httpServer
-			.once('error', (err) => {
-				if (err.code === 'EADDRINUSE') {
-					console.warn(
-						`Port ${port} in use, trying port ${port + 1}`
-					);
-				} else {
-					console.error(err);
-					process.exit(1);
-				}
-			})
-			.listen(port, () => {
-				console.log(`> Ready on http://${hostname}:${port}`);
-			});
-	});
+        httpServer
+            .once("error", (err) => {
+                if (err.code === "EADDRINUSE") {
+                    console.warn(
+                        `Port ${port} in use, trying port ${port + 1}`
+                    );
+                } else {
+                    console.error(err);
+                    process.exit(1);
+                }
+            })
+            .listen(port, () => {
+                console.log(`> Ready on http://${hostname}:${port}`);
+            });
+    });
 };
 
 startServer(port);
